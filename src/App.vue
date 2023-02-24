c6c8818ea84e886f3c5a0f62615bff64ac62d3fb
TheHeart <thenikuman2000@gmail.com>
2023-02-25 01:56:55 +0900

    updeta:  App.vueを書き換え、文字が変わるようになった。

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
-import { ref } from 'vue'
-const message = ref('')
-
+import { ref } from "vue"
+const x = ref(true)
+function y() {
+  x.value = !x.value
+}
 </script>
 
 <template>
-  <input v-model="message" placeholder="edit me" />
-  <button>ボタン</button>
+<div>
+  <button @click="y">文字が変わるよ</button>
+  <h1 v-if="x">もとの文字</h1>
+  <h1 v-else>かわった文字</h1>
+</div>
 </template>
